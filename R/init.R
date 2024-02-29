9e950a9ca7bc6b80f27a110622e641e591d0b20d
Ian Townsend <ian.townsend@gmail.com>
2024-02-29 14:45:21 +0000

    Temporarily remove function comment

--- R/init.R
+++ R/init.R
@@ -1,10 +1,3 @@
-#' Creates a request object for a valid red_list_api_key
-#'
-#' @param red_list_api_key A number.
-#' @returns An httr2 request object.
-#' @examples
-#' init_red_list_api('your_red_list_api_key')
-
 init_red_list_api <- function(red_list_api_key) {
   key <- httr2::secret_make_key()
   my_key <- httr2::secret_encrypt(red_list_api_key, key)
