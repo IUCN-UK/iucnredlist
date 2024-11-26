76fd0367400e69e942f3aad32588518dacb3a752
simon-tarr <simon.tarr@iucn.org>
2024-11-26 16:14:08 +0000

    Changed req -> api for consistency across functions

--- R/assessments_by_group.R
+++ R/assessments_by_group.R
@@ -1,12 +1,12 @@
 # GET BASIC ASSESSMENT INFORMATION BY GROUP AND CODE/NAME
-assessments_by_group <- function(req, group, code, year_published = NULL, latest = TRUE, scope_code = NULL, wait_time = 0.5) {
+assessments_by_group <- function(api, group, code, year_published = NULL, latest = TRUE, scope_code = NULL, wait_time = 0.5) {
 
   # Gather user's query params, throw away any that are NULL
   query_params <- list(latest = latest, year_published = year_published, scope_code = scope_code, page = 1, per_page = 100) %>%
     purrr::discard(is.null)
 
   url <- paste0("https://api.iucnredlist.org/api/v4/", group, "/", code)
-  data <- fetch_paginated_data(req, url, query_params, wait_time = wait_time)
+  data <- fetch_paginated_data(api, url, query_params, wait_time = wait_time)
 
   if (nrow(data) > 0) {
     janitor::clean_names(data) %>%
