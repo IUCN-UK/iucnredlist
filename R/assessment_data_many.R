7cb55f45f0b71d3d698697ca912f045af627ab1e
simon-tarr <simon.tarr@iucn.org>
2024-11-26 16:32:41 +0000

    Added progress bar, changed error icons to warnings

--- R/assessment_data_many.R
+++ R/assessment_data_many.R
@@ -9,14 +9,13 @@
 #' @param assessment_ids Vector. A vector of valid assessment IDs
 #' @param wait_time Time in seconds to wait between API calls. A wait of 0.5 seconds or greater will ensure you
 #' won't hit the IUCN Red List API rate limit.
-#' @param silent Logical. If FALSE (default) then the status of each API call will be printed to the R console.
 #' @returns A list of full assessment data. Each element in the list corresponds to a unique assessment_id passed
 #' to the function via the argument assessment_ids
 #' @examples
 #' \dontrun{
 #' assessment_data_many(api, assessment_ids = c(123, 456), wait_time = 0.5, silent = FALSE)
 #' }
-assessment_data_many <- function(api, assessment_ids, wait_time = 1, silent = FALSE) {
+assessment_data_many <- function(api, assessment_ids, wait_time = 1) {
   # Initialize an empty list to store results
   results_list <- list()
 
@@ -24,9 +23,10 @@ assessment_data_many <- function(api, assessment_ids, wait_time = 1, silent = FA
   assessment_ids <- unique(assessment_ids)
 
   if (wait_time < 0.5) {
-    cli::cli_alert_danger(paste0("Waiting for ", wait_time, " seconds between API calls.
-                                This is a short wait time and could result in your API token being rate limited.
-                                Please consider increasing your wait time to >=0.5 seconds between calls to maintain service reliablity for all."))
+    cli::cli_alert_warning(paste0("Waiting for ", wait_time, " seconds between API calls."))
+    cli::cli_alert_warning(paste0("This is a short wait time and could result in your API token being rate limited."))
+    cli::cli_alert_warning(paste0("Please consider increasing your wait time to >=0.5 seconds between calls to maintain service reliablity for all."))
+
   } else {
     cli::cli_alert_success(paste0("Waiting for ", wait_time, " second(s) between API calls. Thanks for using the API responsibly!"))
   }
@@ -38,17 +38,21 @@ assessment_data_many <- function(api, assessment_ids, wait_time = 1, silent = FA
 
   start <- Sys.time()
 
+  pb <- cli::cli_progress_bar(
+    format = "Progress: {cli::pb_bar} {cli::pb_percent} | ETA: {cli::pb_eta}",
+    total = length(assessment_ids),
+    clear = TRUE
+  )
+
   for (i in seq_along(assessment_ids)) {
-    # Fetch and process the data for this assessment ID
     results_list[[i]] <- assessment_data(api, assessment_ids[i])
-    if (silent == FALSE) {
-      print(paste("Processed assessment ID:", assessment_ids[i]))
-    }
+    cli::cli_progress_update(pb, set = i)
     Sys.sleep(wait_time)
   }
 
-  end <- Sys.time()
+  cli::cli_progress_done(pb)
 
+  end <- Sys.time()
 
   cli::cli_alert_success(paste0("Assessment data finished downloading in ", round(difftime(end, start, units = "secs"), 2), " seconds."))
 
