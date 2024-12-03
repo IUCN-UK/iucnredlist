c865392fa61a39d7c86136c06dde9f4dfc6b6a65
Ian Townsend <ian.townsend@gmail.com>
2024-12-03 11:38:40 +0000

    Update test to extract accessed on date

--- tests/testthat/assessment_invalid_api_key/api.iucnredlist.org/api/v4/assessment/742738.R
+++ tests/testthat/assessment_invalid_api_key/api.iucnredlist.org/api/v4/assessment/742738.R
@@ -1,5 +1,5 @@
 structure(list(method = "GET", url = "https://api.iucnredlist.org/api/v4/assessment/742738", 
     status_code = 401L, headers = structure(list(`cache-control` = "no-cache", 
-        `content-type` = "application/json", `x-request-id` = "7e781951-addf-4857-9a4e-a6ac3b1a9884", 
-        `x-runtime` = "0.005483", `content-length` = "24", date = "Mon, 02 Dec 2024 21:57:33 GMT"), class = "httr2_headers"), 
+        `content-type` = "application/json", `x-request-id` = "fa1cf364-8227-437e-b1df-d2b230f89c4b", 
+        `x-runtime` = "0.005559", `content-length` = "24", date = "Tue, 03 Dec 2024 11:07:43 GMT"), class = "httr2_headers"), 
     body = charToRaw("{\"error\":\"Unauthorized\"}"), cache = new.env(parent = emptyenv())), class = "httr2_response")
