58d45279d6b8cb2ffe91c25bd1047c3ecc245395
Ian Townsend <ian.townsend@gmail.com>
2024-12-05 10:44:10 +0000

    Run styler

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -2,15 +2,15 @@ test_that("perform_request successfuly requests an assessment from the API and r
   httptest2::with_mock_dir("assessment_742738", {
     red_list_api_key <- Sys.getenv("RED_LIST_API_KEY")
     api <- init_api(red_list_api_key)
-    expect_length(perform_request(api, 'assessment/742738'), 33)
+    expect_length(perform_request(api, "assessment/742738"), 33)
   })
 })
 
 test_that("perform_request handles a 401 error for an invalid API key", {
   httptest2::with_mock_dir("assessment_invalid_api_key", {
-    api <- init_api('an_invalid_api_key')
+    api <- init_api("an_invalid_api_key")
     expect_error(
-      perform_request(api, 'assessment/742738'),
+      perform_request(api, "assessment/742738"),
       "Error 401: Unauthorized. This error may occur if your Red List API token was copied incorrectly."
     )
   })
@@ -21,7 +21,7 @@ test_that("perform_request handles a non 200 or 401 http status code (e.g. for a
     red_list_api_key <- Sys.getenv("RED_LIST_API_KEY")
     api <- init_api(red_list_api_key)
     expect_error(
-      perform_request(api, 'invalid_endpoint/742738'),
+      perform_request(api, "invalid_endpoint/742738"),
       "An unexpected error occurred: HTTP 404 Not Found."
     )
   })
