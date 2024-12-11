0b2d78cc48ab140efa09e15886ee1ca5d4a927c2
Ian Townsend <iant@users.noreply.github.com>
2024-12-11 13:11:40 +0000

    Add a user agent to identify the package and version (#41)

--- tests/testthat/assessment_invalid_api_key/api.iucnredlist.org/api/v4/assessment/742738.R
+++ tests/testthat/assessment_invalid_api_key/api.iucnredlist.org/api/v4/assessment/742738.R
@@ -1,5 +1,5 @@
 structure(list(method = "GET", url = "https://api.iucnredlist.org/api/v4/assessment/742738", 
     status_code = 401L, headers = structure(list(`cache-control` = "no-cache", 
-        `content-type` = "application/json", `x-request-id` = "fa1cf364-8227-437e-b1df-d2b230f89c4b", 
-        `x-runtime` = "0.005559", `content-length` = "24", date = "Tue, 03 Dec 2024 11:07:43 GMT"), class = "httr2_headers"), 
+        `content-type` = "application/json", `x-request-id` = "59f1c7e3-6b78-4393-8ae0-9c9e4becfab7", 
+        `x-runtime` = "0.005139", `content-length` = "24", date = "Wed, 11 Dec 2024 12:39:23 GMT"), class = "httr2_headers"), 
     body = charToRaw("{\"error\":\"Unauthorized\"}"), cache = new.env(parent = emptyenv())), class = "httr2_response")
