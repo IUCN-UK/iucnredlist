9e11727c2bf98c7c248f6eb07ce83c10a55b2cf8
Ian Townsend <ian.townsend@gmail.com>
2024-12-11 12:39:38 +0000

    Ooops

---
+++ tests/testthat/assessment_invalid_api_key/api.iucnredlist.org/api/v4/assessment/742738.R
@@ -0,0 +1,5 @@
+structure(list(method = "GET", url = "https://api.iucnredlist.org/api/v4/assessment/742738", 
+    status_code = 401L, headers = structure(list(`cache-control` = "no-cache", 
+        `content-type` = "application/json", `x-request-id` = "59f1c7e3-6b78-4393-8ae0-9c9e4becfab7", 
+        `x-runtime` = "0.005139", `content-length` = "24", date = "Wed, 11 Dec 2024 12:39:23 GMT"), class = "httr2_headers"), 
+    body = charToRaw("{\"error\":\"Unauthorized\"}"), cache = new.env(parent = emptyenv())), class = "httr2_response")
