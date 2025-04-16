6498646bfda2aa2b9ffc15a7cf2a449cb1b37f55
KateHarding <kate.harding@iucn.org>
2025-04-16 18:12:01 +0200

    I think this should fix the error I was getting

--- tests/testthat/gss_data_all/api.iucnredlist.org/api/v4/green_status/all.R
+++
@@ -1,6 +0,0 @@
-structure(list(method = "GET", url = "https://api.iucnredlist.org/api/v4/green_status/all", 
-    status_code = 403L, headers = structure(list(`Cache-Control` = "no-cache", 
-        `Content-Length` = "21", `Content-Type` = "application/json", 
-        `X-Request-Id` = "e11c1c39-822d-481f-a73f-a62770656d93", 
-        `X-Runtime` = "0.003954", Date = "Fri, 11 Apr 2025 13:24:03 GMT"), redact = character(0), class = "httr2_headers"), 
-    body = charToRaw("{\"error\":\"Forbidden\"}"), cache = new.env(parent = emptyenv())), class = "httr2_response")
