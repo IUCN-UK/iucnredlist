ce169fb5662adb58afa1eeb08188e5f75a92531e
KateHarding <59656592+KateHarding@users.noreply.github.com>
2025-04-23 16:40:11 +0200

    Add function to pull all gss assessment data from the v4 API (#52)
    Created function to pull all gss assessment data from the iucnredlist v4 API and parse to tibble
    * Fetch gss data 
    * Parse it to tibble, omitting some fields. 
    * Plus tests.

---
+++ R/gss_data_all.R
@@ -0,0 +1,20 @@
+#' Fetch all green status of species (gss) assessment data
+#'
+#' @description Returns full assessment data for all green status of species assessments.
+#'
+#' @param api An `{httr2}` response object created with `init_api()`.
+#'
+#' @returns Returns an unprocessed named list of assessment data for all
+#' green status of species assessments.
+#' The raw JSON from the gss endpoint has been coerced to a list but is otherwise
+#' an exact representation of the response from the assessment endpoint.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' gss_data_all(api)
+#' }
+gss_data_all <- function(api) {
+  endpoint_request <- "green_status/all"
+  perform_request(api, endpoint_request)
+}
---
+++ tests/testthat/test-gss_data_all.R
@@ -0,0 +1,29 @@
+test_that("gss_data_all calls perform_request and returns expected response", {
+  httptest2::with_mock_dir("gss_data_all", {
+    red_list_api_key <- Sys.getenv("RED_LIST_API_KEY")
+    api <- init_api(red_list_api_key)
+
+    raw_gss_result <- gss_data_all(api)
+
+    expect_true(is.list(raw_gss_result))
+    expect_length(raw_gss_result, 1)
+    expect_length(unique(raw_gss_result$assessments), 115) # 115 assessments returned
+
+    expect_equal(names(raw_gss_result), c("assessments"))
+
+    expect_equal(names(raw_gss_result$assessments[[1]]), c(
+      "assessment_year", "weights", "justification",
+      "species_recovery_category", "species_recovery_score_best", "species_recovery_score_minimum",
+      "species_recovery_score_maximum", "conservation_legacy_category", "conservation_legacy_best",
+      "conservation_legacy_minimum", "conservation_legacy_maximum", "conservation_dependence_category",
+      "conservation_dependence_best", "conservation_dependence_minimum", "conservation_dependence_maximum",
+      "conservation_gain_category", "conservation_gain_best", "conservation_gain_minimum",
+      "conservation_gain_maximum", "recovery_potential_category", "recovery_potential_best",
+      "recovery_potential_minimum", "recovery_potential_maximum", "assessor_names",
+      "reviewer_names", "contributors", "facilitators",
+      "compilers", "url", "assessment_date",
+      "taxon"
+    )) # Column headers returned for each assessment
+
+  })
+})
